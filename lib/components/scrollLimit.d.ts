2125b299182e235212269e39fb2fc20c9ab2f568
nigel <nigel.c.king@gmail.com>
2020-04-21 18:16:26 +0100

    build commited

---
+++ lib/components/scrollLimit.d.ts
@@ -0,0 +1,11 @@
+import { Limit } from './limit';
+declare type Params = {
+    contentSize: number;
+    loop: boolean;
+    isContained: boolean;
+};
+export declare type ScrollLimit = {
+    measure: (scrollSnaps: number[]) => Limit;
+};
+export declare function ScrollLimit(params: Params): ScrollLimit;
+export {};
